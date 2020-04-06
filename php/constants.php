63872bea26382a8251bc29fea0f5605eb5bd8db0
Alharthy,Ahmed A.S.A <425113@student.fontys.nl>
2020-04-06 12:08:12 +0400

    Deleted loginphp.php and small fixes in loginLogic.php

--- php/constants.php
+++ php/constants.php
@@ -1,4 +1,6 @@
 <?php
+//variable constants
+$WORKER_TYPE = 2;
 // routes
 $PAGENAME_LOGIN = "login.php";
 $BASE_URL = "/mediabazarweb/html/";
--- php/loginLogic.php
+++ php/loginLogic.php
@@ -1,19 +1,21 @@
 <?php
+include 'constants.php';
 include 'config.php';
-session_start();
+if(session_status() == PHP_SESSION_NONE)
+{
+  session_start();
+}
 if (isset($_SESSION["username"])) {
-    echo " <meta http-equiv='refresh' content='0; url=../html/base.php'>";
+    header("Location: ../html/base.php");
 } else {
     if (isset($_POST["login_bttn"])) {
         $errorMessageText = '';
-        $errorMessage = 0;
         $username = $_POST['username'];
         $password = $_POST['password'];
         if (empty($username) || empty($password)) {
             $errorMessageText = 'Please fill in the empty fields';
-            $errorMessage = 1;
         } else {
-            $query = "SELECT * FROM user WHERE username = '$username' AND password = '$password' AND account_type = 2";
+            $query = "SELECT * FROM user WHERE username = '$username' AND password = '$password' AND account_type = $WORKER_TYPE";
             $statment = $conn->prepare($query);
             $statment->execute(
                 array(
@@ -24,11 +26,9 @@ if (isset($_SESSION["username"])) {
             $count = $statment->rowCount();
             if ($count > 0) {
                 $_SESSION["username"] = $_POST["username"];
-                setcookie("logedin", 1, time() + (3600 * 24));
-                header("location:../html/base.php");
+                header("Location: ../html/base.php");
             } else {
                 $errorMessageText = 'Wrong credentials';
-                $errorMessage = 1;
             }
         }
     }
