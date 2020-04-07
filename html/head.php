0ae86baa06affca07f996ba27373d4d08a01dc6a
Karina Kozarova <karinakozarova1303@gmail.com>
2020-04-07 13:57:01 +0200

    added basic design

--- html/head.php
+++ html/head.php
@@ -12,6 +12,11 @@ session_start();
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 
+    <?php if($addCalendar) { ?>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <link rel="stylesheet" href="../libs/jquery-sked-tape-master/docs/jquery.skedTape.css">
+        <script src="../libs/jquery-sked-tape-master/src/jquery.skedTape.js"></script>
+    <?php } ?>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 </head>
