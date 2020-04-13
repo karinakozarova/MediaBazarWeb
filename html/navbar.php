5165b627c9382ef43d2ffadbb4363f6c27e665f9
Evgeniy Terziev <i425986@fhict.nl>
2020-04-14 01:03:18 +0300

    Fixed issues

--- html/navbar.php
+++ html/navbar.php
@@ -27,7 +27,7 @@
 
             <ul class="nav navbar-nav navbar-right">
                 <li><a href="../html/inbox.php">Inbox <span class="badge" id="notificationNumber"></span></a></li>
-                <script type="text/javascript" src="../js/updateNotificationsCount.js"></script>
+                <script type="text/javascript" src="../js/updateNotificationsCount.js">0</script>
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
                        aria-expanded="false"><?= strtoupper($_SESSION["username"]) ?> <span class="caret"></span></a>
