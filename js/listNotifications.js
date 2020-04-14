1361b73ca64b0def5cde1630b1cb380afbd13f76
Evgeniy Terziev <i425986@fhict.nl>
2020-04-14 16:18:33 +0300

    Fixed commented issues

--- js/listNotifications.js
+++ js/listNotifications.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    var notificationsIdArray = [];
+    var notificationsId = [];
     var notifications = [];
     setInterval(function() {
         $.ajax({
@@ -11,14 +11,14 @@ $(document).ready(function() {
             }
         });
         for (i = 0; i < notifications.length; i++) {
-            var flag = notificationsIdArray.includes(notifications[i].notificationId);
-            if(!flag){
+            var isIncluded = notificationsId.includes(notifications[i].notificationId);
+            if(!isIncluded){
                 if(notifications[i].notificationStatus == "read"){
                     $(".notifications").append('<div class="container"> <div class="alert alert-success" role="alert"> <form method="post"> <input type="hidden" name="notificationID" value="' + notifications[i].notificationId + '"></form><h4 class="alert-heading">Greetings!</h4> <p>' + notifications[i].message + '</p><hr> <p class="mb-0"><b>Created by: ' + notifications[i].creatorFirstName + ' '+ notifications[i].creatorLastName +'</b></p><p class="mb-0"><b>Date: ' + notifications[i].date + '</b></p></div></div>');
                 }else if (notifications[i].notificationStatus == "unread"){
                     $(".notifications").append('<div class="container"> <div class="alert alert-danger ' + notifications[i].notificationId +'" role="alert"><form method="post"><input type="hidden" name="notificationID" value="' + notifications[i].notificationId + '"><button type="button" name="markAsRead" value="' + notifications[i].notificationId + '" class="close">Mark as read</button></form> <h4 class="alert-heading">Greetings!</h4> <p>' + notifications[i].message + '</p><hr> <p class="mb-0">Created by: ' + notifications[i].creatorFirstName + ' '+ notifications[i].creatorLastName +'</p><p class="mb-0">Date: ' + notifications[i].date + '</p></div></div>');
                 }
-                notificationsIdArray.push(notifications[i].notificationId);
+                notificationsId.push(notifications[i].notificationId);
             }
         }
         $(".close").click(function() {
