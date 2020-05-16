524260012096cfc44529a1756ae70aab438ec841
Karina Kozarova <karinakozarova1303@gmail.com>
2020-05-16 22:56:52 +0200

    add credits page

---
+++ html/credits.php
@@ -0,0 +1,16 @@
+<?php $title = 'Credits'; ?>
+<?php
+include('head.php');
+?>
+<body>
+<?php include('navbar.php'); ?>
+<link rel="stylesheet" href="../css/credits.css">
+        <div class="sign h1">
+            Thank you for supporting us! <br>
+             - Digital Tech
+        </div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</body>
+</html>
+
