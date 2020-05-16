58ab5111e1b9db6a6e55887372769a880a30d503
Karina Kozarova <karinakozarova1303@gmail.com>
2020-05-16 23:09:08 +0200

    created design

---
+++ html/birthdays.php
@@ -0,0 +1,124 @@
+<?php $title = 'MediaBazar'; ?>
+<?php
+include('head.php');
+?>
+<body>
+<?php include('navbar.php'); ?>
+<link rel="stylesheet" href="../css/birtdays.css">
+<h2 class="centered gradient-text">This month's birthdays <i class="fas fa-birthday-cake"></i> </h2>
+
+<ul class="cards">
+        <li class="cards_item">
+            <div class="card">
+                <div class="card_content">
+                    <h2 class="card_title">Klark Kent </h2>
+                    <h5 class="card_title">(06th October) </h5>
+                    <p class="card_text">Works at Gardening </p>
+                </div>
+            </div>
+        </li>
+        <li class="cards_item">
+            <div class="card">
+                <div class="card_content">
+                    <h2 class="card_title">Klark Kent </h2>
+                    <h5 class="card_title">(06th October) </h5>
+                    <p class="card_text">Works at Gardening </p>
+                </div>
+            </div>
+        </li>
+        <li class="cards_item">
+            <div class="card">
+                <div class="card_content">
+                    <h2 class="card_title">Klark Kent </h2>
+                    <h5 class="card_title">(06th October) </h5>
+                    <p class="card_text">Works at Gardening </p>
+                </div>
+            </div>
+        </li>
+        <li class="cards_item">
+            <div class="card">
+                <div class="card_content">
+                    <h2 class="card_title">Klark Kent </h2>
+                    <h5 class="card_title">(06th October) </h5>
+                    <p class="card_text">Works at Gardening </p>
+                </div>
+            </div>
+        </li>
+        <li class="cards_item">
+            <div class="card">
+                <div class="card_content">
+                    <h2 class="card_title">Klark Kent </h2>
+                    <h5 class="card_title">(06th October) </h5>
+                    <p class="card_text">Works at Gardening </p>
+                </div>
+            </div>
+        </li>
+        <li class="cards_item">
+            <div class="card">
+                <div class="card_content">
+                    <h2 class="card_title">Klark Kent </h2>
+                    <h5 class="card_title">(06th October) </h5>
+                    <p class="card_text">Works at Gardening </p>
+                </div>
+            </div>
+        </li>
+        <li class="cards_item">
+            <div class="card">
+                <div class="card_content">
+                    <h2 class="card_title">Klark Kent </h2>
+                    <h5 class="card_title">(06th October) </h5>
+                    <p class="card_text">Works at Gardening </p>
+                </div>
+            </div>
+        </li><li class="cards_item">
+            <div class="card">
+                <div class="card_content">
+                    <h2 class="card_title">Klark Kent </h2>
+                    <h5 class="card_title">(06th October) </h5>
+                    <p class="card_text">Works at Gardening </p>
+                </div>
+            </div>
+        </li><li class="cards_item">
+            <div class="card">
+                <div class="card_content">
+                    <h2 class="card_title">Klark Kent </h2>
+                    <h5 class="card_title">(06th October) </h5>
+                    <p class="card_text">Works at Gardening </p>
+                </div>
+            </div>
+        </li>
+        <li class="cards_item">
+            <div class="card">
+                <div class="card_content">
+                    <h2 class="card_title">Klark Kent </h2>
+                    <h5 class="card_title">(06th October) </h5>
+                    <p class="card_text">Works at Gardening </p>
+                </div>
+            </div>
+        </li><li class="cards_item">
+            <div class="card">
+                <div class="card_content">
+                    <h2 class="card_title">Klark Kent </h2>
+                    <h5 class="card_title">(06th October) </h5>
+                    <p class="card_text">Works at Gardening </p>
+                </div>
+            </div>
+        </li>
+        <li class="cards_item">
+            <div class="card">
+                <div class="card_content">
+                    <h2 class="card_title">Klark Kent </h2>
+                    <h5 class="card_title">(06th October) </h5>
+                    <p class="card_text">Works at Gardening </p>
+                </div>
+            </div>
+        </li>
+
+
+
+    </ul>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</body>
+</html>
