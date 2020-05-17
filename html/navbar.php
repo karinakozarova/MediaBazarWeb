c6b827aef401c5e819b5ac2f3f5b3ba4000fb87a
Karina Kozarova <karinakozarova1303@gmail.com>
2020-05-17 16:32:13 +0200

    security fix

--- html/navbar.php
+++ html/navbar.php
@@ -1,5 +1,10 @@
 <?php
 include '../php/constants.php';
 
+if(!isset($_SESSION["account_type"])){
+    header("Location: ../html/index.html");
+    exit;
+}
+
 if($_SESSION["account_type"] == $WORKER_TYPE) include('base-navbar.php');
 else include('other-users/navbar.php');
