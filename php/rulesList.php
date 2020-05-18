3f3973be6505366efc880aae832b49bc1deac658
Alharthy,Ahmed A.S.A <425113@student.fontys.nl>
2020-05-18 11:12:28 +0400

    Change file name and tiny fixes to follow the new database name

--- php/rulesList.php
+++ php/rulesList.php
@@ -1,10 +1,10 @@
 <?php
 $rules = [];
 
-$querylist = $conn->prepare("SELECT Description FROM rules");
+$querylist = $conn->prepare("SELECT description FROM rules");
 $querylist->execute();
 $listingRules = $querylist->fetchAll();
 
 foreach ($listingRules as $element) {
-    array_push($rules, $element["Description"]);
+    array_push($rules, $element["description"]);
 }
