4e32b9f3f5340d1891defaf53756d73a9071c400
i425926 <g.minchev@student.fontys.nl>
2020-05-23 10:27:13 +0300

    fixed design

--- html/settings.php
+++ html/settings.php
@@ -8,30 +8,43 @@ include '../php/sendShiftsEmail.php';
 <?php include('navbar.php'); ?>
 <link rel="stylesheet" href="../css/settings.css">
 <br>
-<div class="container">
+<div class="week-container container-fluid">
     <div class="row">
-            <div class="changeInfoForm form-group row col-sm-6 col-md-offset-4 col-lg-offset-3 col-lg-6 col-xl-6">
-                    <div class="form-group">
-                        <h3 class="changeInfoLbl centered">Settings</h3>
+        <div class="col">
+            <div class="card shadow">
+                <div class="card-header py-3 flex justify-content-between">
+                    <div class="row">
+                        <div class="col">
+                            <div class="form-group mb-2">
+                                <h5 class="m-0 font-weight-bold text-info">Settings</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-body">
                         <div class="row">
+                            <div class="col-md-4"></div>
                             <form class="settings-form form-inline" method="POST" id="dark-mode-form">
-                                <div class="col-md-3">
-                                    <h4>Dark Mode</h4>
+                                <div class="form-group">
+                                    <h4 class="text-info">Dark Mode</h4>
                                 </div>
-                                <div class="col-md-3">
+                                <div class="switch-darkmode form-group">
                                     <label id="switch" class="switch">
                                         <input type="checkbox" data-toggle="toggle" name="mode" id="mode">
                                         <span class="slider round"></span>
                                     </label>
                                 </div>
                             </form>
+                            <div class="col"></div>
                         </div>
+                   <br><br><br>
                         <div class="row">
+                            <div class="col-md-4"></div>
                             <form class="settings-form-email form-inline"  method="POST" id="send-email-form">
-                                <div class="col-md-7">
-                                    <h4>Send shift notifications by email</h4>
+                                <div class="form-group">
+                                    <h4 class="text-info">Send shift notifications by email</h4>
                                 </div>
-                                <div class="col-md-3">
+                                <div class="switch-email form-group">
                                     <label id="switch" class="switch">
                                         <input type="checkbox" data-toggle="toggle" name="sendEmail" id="sendEmail">
                                         <span class="slider round"></span>
@@ -39,9 +52,12 @@ include '../php/sendShiftsEmail.php';
                                 </div>
                                 <input type="hidden" id="sendEmail_hidden" name="sendEmail_hidden" value="on">
                             </form>
+                            <div class="col"></div>
                         </div>
-                    </div>
+                    <br><br><br>
+                </div>
             </div>
+        </div>
     </div>
 </div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
